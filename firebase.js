8809a84654a11b3fadecfe34f8b3a048bc64cdb9
Keenly <keenlyhere@gmail.com>
2024-08-12 02:10:42 -0700

    Update firebase config

--- firebase.js
+++ firebase.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import getFirestore from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAzzSJLwus3mwHOXrlKa10Ki2oPzgoKhB8",
-  authDomain: "memoorize-faf57.firebaseapp.com",
-  projectId: "memoorize-faf57",
-  storageBucket: "memoorize-faf57.appspot.com",
-  messagingSenderId: "125332279530",
-  appId: "1:125332279530:web:950557af79328015700793"
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID
 };
 
 // Initialize Firebase
