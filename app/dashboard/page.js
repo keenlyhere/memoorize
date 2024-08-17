c7cf8b6764df4297d0f66579c38e784c7a2cc3aa
Keenly <keenlyhere@gmail.com>
2024-08-17 00:50:28 -0700

    Fixed: wonky cards on dashboard

--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -88,74 +88,75 @@ export default function Dashboard() {
     return (
         <MainLayout>
             {isActivityLoaded ? (
-                <>
+                <div className="w-full h-full">
                     <div className="p-3"></div>
                     <div className="flex justify-between items-center">
                         <h1 className="text-xl font-medium text-dark-gray py-3">Dashboard</h1>
                     </div>
 
-                    {/* stats overview */}
-                    <div className="grid grid-cols-1 md:grid-cols-3 gap-6 mb-6 h-40">
-                        <div className="bg-white p-6 rounded-lg border flex flex-col">
-                            <h2 className="text-lg font-semibold text-dark-gray">Total Courses</h2>
-                            <div className="grow content-center">
-                                <p className="text-6xl font-bold text-primary-purple">
-                                    {courses.length}
-                                </p>
+                    <div className="w-full flex flex-col lg:flex-row lg:flex-wrap lg:justify-between gap-6">
+                        {/* stats overview */}
+                            <div className="bg-white p-6 rounded-lg border flex flex-col w-full lg:basis-[31%]">
+                                <h2 className="text-lg font-semibold text-dark-gray">Total Courses</h2>
+                                <div className="grow content-center">
+                                    <p className="text-6xl font-bold text-primary-purple">
+                                        {courses.length}
+                                    </p>
+                                </div>
                             </div>
-                        </div>
-                        <div className="bg-white p-6 rounded-lg border flex flex-col">
-                            <h2 className="text-lg font-semibold text-dark-gray">Total Flashcard Sets</h2>
-                            <div className="grow content-center">
-                                <p className="text-6xl font-bold text-primary-purple">
-                                    { totalSets }
-                                </p>
+                            <div className="bg-white p-6 rounded-lg border flex flex-col w-full lg:basis-[31%]">
+                                <h2 className="text-lg font-semibold text-dark-gray">Total Flashcard Sets</h2>
+                                <div className="grow content-center">
+                                    <p className="text-6xl font-bold text-primary-purple">
+                                        { totalSets }
+                                    </p>
+                                </div>
                             </div>
-                        </div>
-                        <div className="bg-white p-6 rounded-lg border flex flex-col">
-                            <h2 className="text-lg font-semibold text-dark-gray">Total Flashcards</h2>
-                            <div className="grow content-center">
-                                <p className="text-6xl font-bold text-primary-purple">
-                                    { totalFlashcards }
-                                </p>
+                            <div className="bg-white p-6 rounded-lg border flex flex-col w-full lg:basis-[31%]">
+                                <h2 className="text-lg font-semibold text-dark-gray">Total Flashcards</h2>
+                                <div className="grow content-center">
+                                    <p className="text-6xl font-bold text-primary-purple">
+                                        { totalFlashcards }
+                                    </p>
+                                </div>
                             </div>
-                        </div>
+                            {/* upcoming reviews */}
+                            <div className="bg-white p-6 rounded-lg border flex flex-col w-full lg:w-[30%]">
+                                <h2 className="text-lg font-semibold text-dark-gray">Flashcards due today:</h2>
+                                <div className="grow content-center">
+                                    <p className="text-6xl font-bold text-primary-purple">
+                                        { flashcardsDueToday }
+                                    </p>
+                                </div>
+                            </div>
+                            {/* Recent Activity */}
+                            <div className="bg-white p-6 rounded-lg border mb-6 h-full lg:mb-0 lg:grow">
+                                <h2 className="text-lg font-semibold text-dark-gray mb-3">Recent Activity</h2>
+                                <div className="grow content-center">
+                                    <ul className="space-y-3">
+                                        {recentActivity.length > 0 ? (
+                                            recentActivity.map((activity, index) => (
+                                                <li key={index} className="flex justify-between">
+                                                    <span className="text-gray-500">
+                                                        {activity.activity}
+                                                    </span>
+                                                    <span className="text-muted-purple">
+                                                        {new Date(activity.lastReviewedAt).toLocaleDateString()}
+                                                    </span>
+                                                </li>
+                                            ))
+                                        ) : (
+                                            <p>No recent activity</p>
+                                        )}
+                                    </ul>
+                                </div>
+                            </div>
                     </div>
 
-                    <div className="grid grid-cols-1 md:grid-cols-3 gap-6 mb-6 h-40 w-full">
-                        {/* upcoming reviews */}
-                        <div className="bg-white p-6 rounded-lg border mb-6 col-span-1 h-full">
-                            <h2 className="text-lg font-semibold text-dark-gray">Flashcards due today:</h2>
-                            <div className="grow content-center">
-                                <p className="text-6xl font-bold text-primary-purple">
-                                    { flashcardsDueToday }
-                                </p>
-                            </div>
-                        </div>
-                        {/* Recent Activity */}
-                        <div className="bg-white p-6 rounded-lg border mb-6 h-full col-span-2">
-                            <h2 className="text-lg font-semibold text-dark-gray mb-3">Recent Activity</h2>
-                            <div className="grow content-center">
-                                <ul className="space-y-3">
-                                    {recentActivity.length > 0 ? (
-                                        recentActivity.map((activity, index) => (
-                                            <li key={index} className="flex justify-between">
-                                                <span className="text-gray-500">
-                                                    {activity.activity}
-                                                </span>
-                                                <span className="text-muted-purple">
-                                                    {new Date(activity.lastReviewedAt).toLocaleDateString()}
-                                                </span>
-                                            </li>
-                                        ))
-                                    ) : (
-                                        <p>No recent activity</p>
-                                    )}
-                                </ul>
-                            </div>
-                        </div>
+
+                    <div className="h-20 w-full lg:hidden">
                     </div>
-                </>
+                </div>
             ) : (
                 <p>Loading...</p>
             )}
